9ae73ea95a674aa6d7bae18b21b245819bdb2c6a
Luana Herllain Lopes <elvis.lopes@al.infnet.edu.br>
2022-03-21 14:36:44 -0300

    Implementado metodo incluir usuário

--- VenturaHr-Api-Usuario/src/main/java/br/edu/infnet/venturahr/apiusuario/model/repository/UsuarioRepository.java
+++ VenturaHr-Api-Usuario/src/main/java/br/edu/infnet/venturahr/apiusuario/model/repository/UsuarioRepository.java
@@ -1,4 +1,14 @@
 package br.edu.infnet.venturahr.apiusuario.model.repository;
 
-public class UsuarioRepository {
+import br.edu.infnet.venturahr.apiusuario.model.domain.Usuario;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends CrudRepository<Usuario,Integer> {
+    Usuario findByEmail(String email);
+
+    @Query("from Usuario u where u.email = :email and u.senha = :senha")
+    Usuario autenticacao(String email, String senha);
 }
