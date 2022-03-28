524e6f5fcbbb16cd6fce498ce8cc14a627f484e6
Elvis Lopes <evs.lopes@gmail.com>
2022-03-28 07:45:40 -0300

    Adicionado classes de modelo a estrutura MVC: Usuario, Vaga e Criterio

---
+++ VenturaHr-ClienteWeb/src/main/java/br/infnet/edu/venturahr/web/domain/Vaga.java
@@ -0,0 +1,64 @@
+package br.infnet.edu.venturahr.web.domain;
+
+import java.util.List;
+
+public class Vaga {
+    private Integer id;
+    private String cargo;
+    private String cidade;
+    private String tipoDeContrato;
+    private Integer usuarioId;
+    private List<Criterio> CriterioList;
+
+    public Vaga(List<Criterio> criterioList) {
+        CriterioList = criterioList;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getTipoDeContrato() {
+        return tipoDeContrato;
+    }
+
+    public void setTipoDeContrato(String tipoDeContrato) {
+        this.tipoDeContrato = tipoDeContrato;
+    }
+
+    public Integer getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(Integer usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    public List<Criterio> getCriterioList() {
+        return CriterioList;
+    }
+
+    public void setCriterioList(List<Criterio> criterioList) {
+        CriterioList = criterioList;
+    }
+}
